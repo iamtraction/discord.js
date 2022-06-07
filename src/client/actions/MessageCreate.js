71b8fef9984c85fa7d91bbb4bcba4ab7c44ea173
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-06-07 22:17:57 +0530

    actions(MessageCreate): only run in non voice channels

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -7,7 +7,7 @@ class MessageCreateAction extends Action {
   handle(data) {
     const client = this.client;
     const channel = client.channels.cache.get(data.channel_id);
-    if (channel) {
+    if (channel && channel.type !== "voice") {
       const existing = channel.messages.cache.get(data.id);
       if (existing) return { message: existing };
       const message = channel.messages.add(data);
