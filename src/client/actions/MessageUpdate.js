be51d835e9872c43be0d7911a6ee91668682f7c2
TRACTION <19631364+iamtraction@users.noreply.github.com>
2022-06-07 22:20:20 +0530

    actions(MessageUpdate): only run in non voice channels

--- src/client/actions/MessageUpdate.js
+++ src/client/actions/MessageUpdate.js
@@ -5,7 +5,7 @@ const Action = require('./Action');
 class MessageUpdateAction extends Action {
   handle(data) {
     const channel = this.getChannel(data);
-    if (channel) {
+    if (channel && channel.type !== "voice") {
       const { id, channel_id, guild_id, author, timestamp, type } = data;
       const message = this.getMessage({ id, channel_id, guild_id, author, timestamp, type }, channel);
       if (message) {
